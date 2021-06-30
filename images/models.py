306f913cb9ae8f0b4791451226ff576be3bb3200
FITSTARGILLY2643 <fitstargilii1194@gmail.com>
2021-06-30 23:22:08 +0300

    Create the Image model and it's methods

--- images/models.py
+++ images/models.py
@@ -35,5 +35,42 @@ class Category(models.Model):
         
     def __str__(self):
         return self.name
+
+class Image(models.Model):
+    image = CloudinaryField('image')
+    description = models.TextField()
+    author = models.CharField(max_length=55, default='admin')
+    date = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=55)    
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete = models.CASCADE)
+
+    def save_image(self):
+        self.save()
+    
+    def delete_image(self):
+        self.delete()
+        
+    @classmethod
+    def update_image(cls, id, value):
+        cls.objects.filter(id=id).update(image=value)
+    
+    @classmethod
+    def get_image_by_id(cls, id):
+        image = cls.objects.filter(id=id).all()
+        return image
+
+    @classmethod
+    def search_by_category(cls, category):
+        images = cls.objects.filter(category__name__contains=category)
+        return images
+    
+    @classmethod
+    def filter_by_location(cls, location):
+        image_location = Image.objects.filter(location__name=location).all()
+        return image_location
+  
+    def __str__(self):
+        return self.name 
     
         
