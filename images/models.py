c78799cf2d32c2f14868d139211200018d1f0811
FITSTARGILLY2643 <fitstargilii1194@gmail.com>
2021-06-30 18:56:03 +0300

    Create the model Location

--- images/models.py
+++ images/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+import cloudinary
+from cloudinary.models import CloudinaryField
+import datetime as dt
 
 # Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length = 55)
