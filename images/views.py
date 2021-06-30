8433dac0e63ed2a0e7ffe03341a62ef8191676eb
FITSTARGILLY2643 <fitstargilii1194@gmail.com>
2021-06-30 23:31:23 +0300

    Create the search_location view function for searching images based on locations

--- images/views.py
+++ images/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-from .models import Image,Location,Category
+from .models import Location,Category,Image
 from django.http import HttpResponse, Http404
 import datetime as dt
 # Create your views here.
@@ -7,4 +7,16 @@ def fits_gallery(request):
     date = dt.date.today()
     images = Image.objects.all()
     return render(request, 'gallery.html', {"date": date,"images":images})
+
+def search_location(request):
+    if 'location' in request.GET and request.GET["location"]:
+        location = request.GET.get("location")
+        searched_images = Image.filter_by_location(location)
+        message = f"{location}"
+        # print("Image.......",searched_images)
+        return render(request, 'location.html', {"message": message, "images": searched_images})
+    else:
+        message = "You haven't searched for any image location"
+        return render(request, 'location.html', {"message": message})
+
 
