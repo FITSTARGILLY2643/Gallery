505970bdecf3d0ee2209e7dd048fa7cff9356e9a
FITSTARGILLY2643 <fitstargilii1194@gmail.com>
2021-07-01 08:30:24 +0300

    Create  the tests for Image model methods

--- images/tests.py
+++ images/tests.py
@@ -25,6 +25,43 @@ class LocationTestClass(TestCase):
         # Testing update location Method
     def test_update_location(self):
         self.location.save_location()
-        self.location.update_location(self.location.id, 'Kiambu')
-        changed_location = Location.objects.filter(name ='Kiambu')
+        self.location.update_location(self.location.id, 'kiambu')
+        changed_location = Location.objects.filter(name ='kiambu')
         self.assertTrue(len(changed_location) > 0) 
+
+class ImageTestClass(TestCase):
+    def setUp(self):
+    # Creating a new location and saving it
+        self.location = Location(id=1,name = '')
+        self.location.save_location()
+
+        # Creating a new category and saving it
+        self.category = Category(id=1,name = 'birthday')
+        self.category.save_category()
+
+        self.new_image= Image(id=1, name='image', description='this is a test image', location=self.location,category=self.category)
+        self.new_image.save()
+
+        # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_image,Image))
+
+         # Testing Save Method
+    def test_save_method(self):
+        self.new_image.save_image()
+        new_image = Image.objects.all()      
+        self.assertTrue(len(new_image) >0)
+
+        #Testing delete function
+    def test_delete_image(self):
+        self.new_image.delete_image()
+        image = Image.objects.all()
+        self.assertTrue(len(image)== 0)
+
+        #Testing image update method
+    def test_update_image(self):
+        self.new_image.save_image()
+        self.new_image.update_image(self.new_image.id, 'images/fits.jpg')
+        changed_img = Image.objects.filter(image='images/fits.jpg')
+        self.assertTrue(len(changed_img) > 0)
+        
